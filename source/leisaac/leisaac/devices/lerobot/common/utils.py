22c0c6ef0969ed3b0bd20a365598732b3f6ffbfd
EverNorif <1320346985@qq.com>
2025-07-04 17:24:25 +0800

    fix: enter pressed in windows platform.

--- source/leisaac/leisaac/devices/lerobot/common/utils.py
+++ source/leisaac/leisaac/devices/lerobot/common/utils.py
@@ -1,8 +1,17 @@
 import select
 import sys
+import platform
 
 def enter_pressed() -> bool:
-    return select.select([sys.stdin], [], [], 0)[0] and sys.stdin.readline().strip() == ""
+    if platform.system() == "Windows":
+        import msvcrt
+
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            return key in (b"\r", b"\n")  # enter key
+        return False
+    else:
+        return select.select([sys.stdin], [], [], 0)[0] and sys.stdin.readline().strip() == ""
 
 
 def move_cursor_up(lines):
