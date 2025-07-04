1a2ceb9cac753b38651c0e83e8b3392f0ed3d9dc
EverNorif <1320346985@qq.com>
2025-07-04 17:48:07 +0800

    fix: add an alternative directory retrieval method.

--- source/leisaac/leisaac/utils/constant.py
+++ source/leisaac/leisaac/utils/constant.py
@@ -1,9 +1,13 @@
 import os
 
-from git import Repo
+try:
+    from git import Repo
 
+    repo = Repo(os.getcwd(), search_parent_directories=True)
+    git_root = repo.git.rev_parse("--show-toplevel")
+except Exception:
+    from pathlib import Path
 
-repo = Repo(os.getcwd(), search_parent_directories=True)
-git_root = repo.git.rev_parse("--show-toplevel")
+    git_root = Path(os.path.abspath(__file__)).parent.parent.parent.parent.parent
 
 ASSETS_ROOT = os.path.join(git_root, 'assets')
